1bcd83714c4c1fb8bff26f1d4087bb8829d82b5d
Chauency <865838279@qq.com>
2017-08-08 23:58:44 +0800

    factorial by recursion

---
+++ factorialByRecursion.c
@@ -0,0 +1,25 @@
+/*************************************************************************
+    > Copyright (C) 2017 All rights reserved.
+    > File Name: factorialByRecursion.c
+    > Author: Chauency
+    > Mail: 865838279@qq.com 
+    > Created Time: 2017-08-08
+    > Description: 
+************************************************************************/
+long factorial(int n)
+{
+	int result = 1;
+	if(n <=0)
+		return 1;
+	else
+		return n * factorial(n-1);
+}
+#include<stdio.h>
+
+int main()
+{
+	int n = 10,resutl = 0;
+	result = factorial(n);
+	printf("%d\n",result);
+    return 0;
+}
