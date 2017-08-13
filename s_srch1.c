5773295e6004346965e5dc7e853ccba021668bdc
Chauency <865838279@qq.com>
2017-08-13 23:45:56 +0800

    Function to find a char in a char string

---
+++ s_srch1.c
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > Copyright (C) 2017 All rights reserved.
+    > File Name: s_srch1.c
+    > Author: Chauency
+    > Mail: 865838279@qq.com 
+    > Created Time: 2017-08-13
+    > Description: 
+ ************************************************************************/
+#include<stdio.h>
+p106
